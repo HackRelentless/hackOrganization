430b63132097beaea6868c3859b7f777fa050248
sciencefreak500 <immunitygame@gmail.com>
2020-05-29 02:14:04 -0400

    added map markers and animations to searching map

--- src/app/chapter-map/chapter-map.component.ts
+++ src/app/chapter-map/chapter-map.component.ts
@@ -7,11 +7,13 @@ import View from 'ol/View';
 import TileLayer from 'ol/layer/Tile';
 import XYZ from 'ol/source/XYZ';
 import {fromLonLat} from 'ol/proj';
-import Vector from 'ol/source/Vector';
 import Feature from 'ol/Feature';
-import Point from 'ol/geom/Point';
-import Style from 'ol/style/Style';
-import { Icon } from 'ol/style';
+import { Circle, Point } from 'ol/geom';
+import VectorLayer from 'ol/layer/Vector';
+import VectorSource from 'ol/source/Vector';
+import { Style, Icon } from 'ol/style';
+import { Coordinate } from 'ol/coordinate';
+
 @Component({
   selector: 'hack-chapter-map',
   templateUrl: './chapter-map.component.html',
@@ -22,27 +24,28 @@ export class ChapterMapComponent implements AfterViewInit {
   map: Map;
   addressLine = '';
 
-  testFeature;
-  hackIcon;
+  addMarker = (coord: Coordinate, isLocation: boolean) => {
+    const iconFeature =  new Feature({
+      geometry: new Point(coord)
+    });
+    const iconStyle = new Style({
+      image: new Icon({
+        src: isLocation ? 'assets/position_marker.png' : 'assets/hack_marker.png',
+        anchor: [0.5, 1],
+        scale: 0.23
+      })
+    });
+    iconFeature.setStyle(iconStyle);
+    return iconFeature;
+  }
+
+  chapterLocationLayer = new VectorSource();
+  positionLayer = new VectorSource();
+
 
   constructor(public http: HttpClient) { }
 
   ngAfterViewInit() {
-
-    // this.testFeature = new Feature({
-    //   geometry: new Point([-75.165222, 39.952583]),
-    //   name: 'Philly Chapter',
-    // });
-
-    // this.hackIcon = new Style({
-    //   image: new Icon({
-    //     anchor: [0.5, 46],
-    //     src: '../../assets/hack_transparent_logo.png'
-    //   })
-    // });
-
-    // this.testFeature.setStyle(this.hackIcon);
-
 
     this.map = new Map({
       target: 'map',
@@ -51,22 +54,41 @@ export class ChapterMapComponent implements AfterViewInit {
           source: new XYZ({
             url: 'https://{a-c}.tile.openstreetmap.org/{z}/{x}/{y}.png'
           })
+        }),
+        new VectorLayer({
+          source: this.chapterLocationLayer
+        }),
+        new VectorLayer({
+          source: this.positionLayer
         })
       ],
       view: new View({
-        center: fromLonLat([-75.165222, 39.952583]),
-        zoom: 15
+        center: fromLonLat([0,0]),
+        zoom: 1
       }),
     });
 
-    // let source = new Vector({});
+    this.chapterLocationLayer.addFeature(
+      this.addMarker(fromLonLat([-75.165222, 39.952583]), false),
+    );
 
   }
 
   findCoordinates() {
     this.http.get('https://nominatim.openstreetmap.org/search?q=' + this.addressLine + '&format=json').subscribe(res => {
       const lookup = res[0];
-      this.map.getView().setCenter(fromLonLat([lookup.lon, lookup.lat]));
+
+      let existingPositions = this.positionLayer.getFeatures();
+      for(let i in existingPositions) {
+        this.positionLayer.removeFeature(existingPositions[i]);
+      }
+      this.positionLayer.addFeature(this.addMarker(fromLonLat([lookup.lon, lookup.lat]), true));
+      
+      this.map.getView().animate({
+        center: fromLonLat([lookup.lon, lookup.lat]),
+        zoom: 13,
+        duration: 1618
+      });
     });
   }
 
