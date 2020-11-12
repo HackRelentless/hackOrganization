6a6bd1389d48e9250fdb44a1485d4989f2cd99c6
sciencefreak500 <immunitygame@gmail.com>
2020-11-11 23:23:37 -0500

    fixed a bug in our event schedule area

--- src/app/general.service.ts
+++ src/app/general.service.ts
@@ -12,7 +12,7 @@ export class GeneralService {
 
   fetchPublicCalendar() {
     return new Promise((resolve) => {
-      let timeMin = moment().startOf('week').subtract(1, 'week').format();
+      let timeMin = moment().startOf('week').subtract(1, 'day').format();
       let timeMax = moment().endOf('week').add(1, 'week').format();
       let publicCalendarURL = `https://clients6.google.com/calendar/v3/calendars/hack@relentlessinnovation.org/events?calendarId=hack@relentlessinnovation.org&singleEvents=true&timeZone=America/New_York&maxAttendees=1&maxResults=250&sanitizeHtml=true&timeMin=${timeMin}&timeMax=${timeMax}&key=AIzaSyBNlYH01_9Hc5S1J9vuFmu2nUqBZJNAXxs`;
       this.http.get(publicCalendarURL, {responseType: 'json'}).subscribe(res => {
