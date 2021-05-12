4c971180b20f7fbcb254e791a14298e6946aa71f
Milord <immunitygame@gmail.com>
2021-05-12 17:18:55 -0400

    join us goes to discord instead

--- src/app/general.service.ts
+++ src/app/general.service.ts
@@ -14,7 +14,7 @@ export class GeneralService {
     return new Promise((resolve) => {
       let timeMin = moment().startOf('week').subtract(1, 'day').format();
       let timeMax = moment().endOf('week').add(1, 'week').format();
-      let publicCalendarURL = `https://clients6.google.com/calendar/v3/calendars/hack@relentlessinnovation.org/events?calendarId=hack@relentlessinnovation.org&singleEvents=true&timeZone=America/New_York&maxAttendees=1&maxResults=250&sanitizeHtml=true&timeMin=${timeMin}&timeMax=${timeMax}&key=AIzaSyBNlYH01_9Hc5S1J9vuFmu2nUqBZJNAXxs`;
+      let publicCalendarURL = `https://clients6.google.com/calendar/v3/calendars/hack@relentlessinnovation.org/events?calendarId=hack%40relentlessinnovation.org&singleEvents=true&timeZone=America%2FNew_York&maxAttendees=1&maxResults=250&sanitizeHtml=true&timeMin=${timeMin}&timeMax=${timeMax}&key=AIzaSyBNlYH01_9Hc5S1J9vuFmu2nUqBZJNAXxs`;
       this.http.get(publicCalendarURL, {responseType: 'json'}).subscribe(res => {
         res['items'] = res['items'].sort((a, b) => {
           return <any>new Date(a.start.dateTime) - <any>new Date(b.start.dateTime)
